252232482ec36368074315c3a91108cc73b3ea63
ramantayal12 <ramantayal12@gmail.com>
2022-08-17 00:05:04 +0530

    Added ArrayList

--- src/main/java/JavaCollections/CollectionsDriver.java
+++ src/main/java/JavaCollections/CollectionsDriver.java
@@ -1,14 +1,24 @@
 package JavaCollections;
 
+import java.util.ArrayList;
+
 public class CollectionsDriver {
 
     public static void main(String args[]){
 
+        // hashMaps
+        System.out.println ("hashMaps");
         OverHashMaps hashMapsBasics = new OverHashMaps ();
         hashMapsBasics.putMethod ();
         hashMapsBasics.getMethod ();
         hashMapsBasics.iterateMethod ();
         hashMapsBasics.forEachMethod ();
+
+        // ArrayList
+        System.out.println ("ArrayList");
+        ArrayListOverView arrayListOverView = new ArrayListOverView ();
+        ArrayList<Integer> arrayList = arrayListOverView.initializeMethods ();
+        arrayListOverView.manipulatingArrayList (arrayList);
 
     }
 }
