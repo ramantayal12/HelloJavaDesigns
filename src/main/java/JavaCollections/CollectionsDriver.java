d64fe69a732bb09c3a77f250576053e227c2698d
ramantayal12 <ramantayal12@gmail.com>
2022-08-16 20:49:16 +0530

    Added HashMaps in Java

---
+++ src/main/java/JavaCollections/CollectionsDriver.java
@@ -0,0 +1,14 @@
+package JavaCollections;
+
+public class CollectionsDriver {
+
+    public static void main(String args[]){
+
+        OverHashMaps hashMapsBasics = new OverHashMaps ();
+        hashMapsBasics.putMethod ();
+        hashMapsBasics.getMethod ();
+        hashMapsBasics.iterateMethod ();
+        hashMapsBasics.forEachMethod ();
+
+    }
+}
