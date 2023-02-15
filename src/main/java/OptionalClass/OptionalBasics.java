41c543f9421213ec60c1eb11150cd3b5b5af0b2b
Raman Tayal <raman.tayal@phonepe.com>
2023-02-15 14:37:43 +0530

    Optional methods addition

--- src/main/java/OptionalClass/OptionalBasics.java
+++ src/main/java/OptionalClass/OptionalBasics.java
@@ -4,6 +4,21 @@ import java.util.Optional;
 
 public class OptionalBasics {
 
+  public static void usingMethodsInsideClassUsingOptional(String words){
+
+    System.out.println(Optional.ofNullable(words)
+        .map(String::toLowerCase)
+        .orElse(null));
+
+    /**
+     * isEmpty returns false so .orElse will be executed
+     */
+    System.out.println(Optional.ofNullable(words)
+        .filter(String::isEmpty)
+        .map(String::toLowerCase)
+        .orElse(null));
+
+  }
   public static void comparingTwoStringsUsingOptional(String s1, String s2 ){
 
     Boolean doesMatch = Optional.ofNullable(s1)
--- src/main/java/SampleDriver.java
+++ src/main/java/SampleDriver.java
@@ -1,10 +1,12 @@
 import static OptionalClass.OptionalBasics.comparingTwoStringsUsingOptional;
+import static OptionalClass.OptionalBasics.usingMethodsInsideClassUsingOptional;
 
 public class SampleDriver {
 
   public static void main(String[] args){
 
     comparingTwoStringsUsingOptional("raman","tayal");
+    usingMethodsInsideClassUsingOptional("Raman");
   }
 
 }
