1a88428e3cbf7f5205f24b4c07cd94e73c02497b
Raman Tayal <raman.tayal@phonepe.com>
2023-05-11 17:10:28 +0530

    adding sha3 methods

--- src/main/java/EncyptionUtils/EncryptionMethods.java
+++ src/main/java/EncyptionUtils/EncryptionMethods.java
@@ -2,18 +2,20 @@ package EncyptionUtils;
 
 import com.google.common.hash.Hashing;
 import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
 import org.apache.commons.codec.digest.DigestUtils;
 
 public class EncryptionMethods {
-//DigestUtils.sha256Hex
-  // Apache
   public static void apacheDigestUtils(){
 
-    System.out.println("Input : Raman " + (DigestUtils.sha256Hex("Raman")));
+    System.out.println("Input : Raman ");
+    System.out.println( (DigestUtils.sha256Hex("Raman")));
 
     System.out.println("Printing if using same input gives us same output");
     System.out.println(DigestUtils.sha256Hex("tayal").equals(DigestUtils.sha256Hex("tayal"))) ;
+
+    String sha256Hex_sha3 = new DigestUtils("SHA3-256").digestAsHex("Raman");
+    System.out.println("Hashing using sha3");
+    System.out.println(sha256Hex_sha3);
 
   }
 
@@ -24,6 +26,7 @@ public class EncryptionMethods {
     String sha256hex2 = Hashing.sha256()
         .hashString("Raman", StandardCharsets.UTF_8)
         .toString();
+    System.out.println("Hashing using google guava ");
     System.out.println(sha256hex);
     System.out.println(sha256hex.equals(sha256hex2));
 
