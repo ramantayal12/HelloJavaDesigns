dcf2e71850f7a0eae18ed290d464569617fb371b
ramantayal12 <ramantayal12@gmail.com>
2023-06-16 23:45:42 +0530

    added implementation of record

--- src/main/java/learnLombokBuilder/ImmutableBuilder.java
+++ src/main/java/learnLombokBuilder/ImmutableBuilder.java
@@ -3,13 +3,29 @@ package learnLombokBuilder;
 
 import lombok.Value;
 
-/*
-Suppose we're using an object that we want to construct with a builder,
-but we can't modify the source or extend the Class.
+/**
+ * Suppose we're using an object that we want to construct with a builder,
+ * but we can't modify the source or extend the Class.
  */
 
 @Value
-final class ImmutableBuilder {
+class ImmutableBuilder {
   int id;
   String name;
 }
+
+/**
+ * Another way of implementing same thing is using record
+ * Basically for records we don't get setters
+ */
+record Student(String name, String address){}
+
+/*
+  a constructor is created for this automatically
+  Person person = new Person("John Doe", "100 Linda Ln.");
+ */
+
+/*
+  getters are also created by default for public variables'
+  person.name()
+ */
