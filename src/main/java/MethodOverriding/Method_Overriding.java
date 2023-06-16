e3e7011d894d1dc024e453f874680f00c2f275a0
ramantayal12 <ramantayal12@gmail.com>
2023-06-17 02:12:40 +0530

    method overloading and overriding

--- src/main/java/MethodOverriding/Method_Overriding.java
+++ src/main/java/MethodOverriding/Method_Overriding.java
@@ -1,16 +1,23 @@
 package MethodOverriding;
 
-// Parent class representing a vehicle
-// this can be implemented using interface also
-class vehicle{
+/**
+ * Vehicle class represents the parent class for vehicles,
+ * containing a run() method that prints a generic message indicating the vehicle is running.
+ *
+ * this can be implemented using interface also
+ */
+class Vehicle {
 
     void run(){
         System.out.println("Vehicle is Running");
     }
 }
 
-// Child class representing a car
-class car extends vehicle{
+/**
+ * Car class is a child class inheriting from vehicle class,
+ * and it overrides the run() method to print a specific message for a car running.
+ */
+class Car extends Vehicle {
 
     int car_number;
     void run(){
@@ -18,25 +25,52 @@ class car extends vehicle{
     }
 }
 
-// Child class representing a bike
-class bike extends vehicle{
+/**
+ * bike class is another child class inheriting from vehicle class,
+ * and it overrides the run() method to print a specific message for a bike running.
+ */
+class Bike extends Vehicle {
 
     void run(){
         System.out.println("Bike is Running");
     }
 }
 
+
+/**
+ * The Method_Overriding class is the entry point of the program.
+ * It creates instances of bike, car, and vehicle classes and calls their respective run() methods.
+ */
 public class Method_Overriding {
 
     public static void main(String []args){
 
-        bike b1 = new bike();
+        Bike b1 = new Bike ();
         b1.run(); // Calling the 'run' method of the 'bike' class
 
-        car c1 = new car();
+        Car c1 = new Car ();
         c1.run(); // Calling the 'run' method of the 'car' class
 
-        vehicle vehicle = new vehicle();
+        Vehicle vehicle = new Vehicle ();
         vehicle.run (); // Calling the 'run' method of the 'vehicle' class
     }
+}
+
+
+/**
+ * The MethodOverLoading class demonstrates method overloading,
+ * where the add() method is overloaded to perform addition with two or three integers.
+ */
+class MethodOverLoading {
+
+    // Method overloading: adds two integers and returns the sum
+    public static int add(int a,int b){
+        return a+b;
+    }
+
+    // Method overloading: adds three integers and returns the sum
+    public static int add(int a,int b,int c){
+        return a+b+c;
+    }
+
 }
