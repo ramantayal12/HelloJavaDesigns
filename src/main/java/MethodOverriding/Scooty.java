e0807dc19e773f99af2bb754a16163253d6e2bfe
ramantayal12 <ramantayal12@gmail.com>
2023-06-17 02:57:35 +0530

    method overriding and overloading rectified

---
+++ src/main/java/MethodOverriding/Scooty.java
@@ -0,0 +1,8 @@
+package MethodOverriding;
+
+
+/**
+ * Scooty class is a child class inheriting from vehicle class,
+ */
+public class Scooty extends Vehicle{
+}
---
+++ src/main/java/MethodOverriding/Vehicle.java
@@ -0,0 +1,14 @@
+package MethodOverriding;
+
+/**
+ * Vehicle class represents the parent class for vehicles,
+ * containing a run() method that prints a generic message indicating the vehicle is running.
+ * <p>
+ * this can be implemented using interface also
+ */
+class Vehicle {
+
+    void run() {
+        System.out.println ("Vehicle is Running");
+    }
+}
