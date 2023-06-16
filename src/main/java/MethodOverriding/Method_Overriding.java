14bdcbbfd9c8f0c16f397b8ee1bdcd0f915b1be9
ramantayal12 <ramantayal12@gmail.com>
2023-06-16 23:49:03 +0530

    added comments in method overidding

--- src/main/java/MethodOverriding/Method_Overriding.java
+++ src/main/java/MethodOverriding/Method_Overriding.java
@@ -1,5 +1,7 @@
 package MethodOverriding;
 
+// Parent class representing a vehicle
+// this can be implemented using interface also
 class vehicle{
 
     void run(){
@@ -7,6 +9,7 @@ class vehicle{
     }
 }
 
+// Child class representing a car
 class car extends vehicle{
 
     int car_number;
@@ -14,6 +17,8 @@ class car extends vehicle{
         System.out.println("Car is Running");
     }
 }
+
+// Child class representing a bike
 class bike extends vehicle{
 
     void run(){
@@ -26,9 +31,12 @@ public class Method_Overriding {
     public static void main(String []args){
 
         bike b1 = new bike();
-        b1.run();
+        b1.run(); // Calling the 'run' method of the 'bike' class
 
         car c1 = new car();
-        c1.run();
+        c1.run(); // Calling the 'run' method of the 'car' class
+
+        vehicle vehicle = new vehicle();
+        vehicle.run (); // Calling the 'run' method of the 'vehicle' class
     }
 }
