fa9b6eb355f01cf77c249f3f1357c46218424f4a
ramantayal12 <ramantayal12@gmail.com>
2023-06-17 00:02:30 +0530

    added comments in lombok packasge

--- src/main/java/learnLombokBuilder/MessageDataAnnotation.java
+++ src/main/java/learnLombokBuilder/MessageDataAnnotation.java
@@ -2,6 +2,12 @@ package learnLombokBuilder;
 
 import lombok.Data;
 
+/**
+ * The @Data annotation from Lombok library generates getter, setter, toString, equals, and hashCode methods
+ * for all fields in the class automatically.
+ * It reduces boilerplate code and improves code readability.
+ * The generated methods ensure encapsulation and provide easy access to the class properties.
+ */
 @Data
 class Message{
 
