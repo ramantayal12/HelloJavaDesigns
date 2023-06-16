8dec6b91eeee3f412019f65010b54feb4c5e1251
ramantayal12 <ramantayal12@gmail.com>
2023-06-17 02:37:29 +0530

    interfaces

--- src/main/java/JavaInterface/PrintableImplementation.java
+++ src/main/java/JavaInterface/PrintableImplementation.java
@@ -1,28 +1,5 @@
 package JavaInterface;
 
-// Interface is blueprint of class
-
-/**
- * The Printable interface serves as a blueprint for classes implementing it.
- * It contains a static constant id and an abstract method print().
- *
- * The interface also includes default methods getUserName() and getUserId() that provide default implementations.
- */
-interface Printable {
-
-    String id = "ID"; // Static constant declaration
-    void print(); // Abstract method declaration
-
-    // Default methods in an interface
-    default String getUserName(){
-        return "default Name";
-    }
-
-    default String getUserId(){
-        return this.id;
-    }
-}
-
 
 /**
  * The PrintableImplementation class implements the Printable interface and
