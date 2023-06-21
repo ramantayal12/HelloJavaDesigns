b72b15d4f11d981a263ae3c4022efa330561deca
Raman Tayal <raman.tayal@phonepe.com>
2023-06-21 14:23:40 +0530

    builder pattern refractored

--- src/main/java/DesignPatterns/BuilderPattern/BuilderPatternDriver.java
+++ src/main/java/DesignPatterns/BuilderPattern/BuilderPatternDriver.java
@@ -1,11 +1,12 @@
 package DesignPatterns.BuilderPattern;
 
+import DesignPatterns.BuilderPattern.BankAccount.BankAccountBuilder;
+
 public class BuilderPatternDriver {
 
     public static void main(String[] args ){
 
-        VehicleBuilderDesignPattern vehicle = new VehicleBuilderDesignPattern
-            .VehicleBuilder()
+        BankAccount bankAccount = new BankAccountBuilder()
             .setAirbags(true)
             .setPrice(200)
             .setName("BuilderPattern.car")
